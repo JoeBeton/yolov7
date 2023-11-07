076df9facc09575160d0d598a98fd0d0ed6fcbdd
JoeBeton <boseph.jeton@gmail.com>
2023-11-07 14:37:08 +0100

    debugging

--- train.py
+++ train.py
@@ -62,6 +62,7 @@ def train(hyp, opt, device, tb_writer=None):
     init_seeds(2 + rank)
     with open(opt.data) as f:
         data_dict = yaml.load(f, Loader=yaml.SafeLoader)  # data dict
+        print(data_dict)
     is_coco = opt.data.endswith('coco.yaml')
 
     # Logging- Doing this before checking the dataset. Might update data_dict
