c80271201bc921250b871a0c517fabbcc5ddc97d
JoeBeton <boseph.jeton@gmail.com>
2023-11-07 15:19:50 +0100

    more debugging

--- utils/datasets.py
+++ utils/datasets.py
@@ -672,10 +672,9 @@ def load_image(self, index):
     img = self.imgs[index]
     if img is None:  # not cached
         path = self.img_files[index]
-        img = cv2.imread(path)  # BGR
-        print(img.shape)
+        img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)[..., np.newaxis]  # BGR
         assert img is not None, 'Image Not Found ' + path
-        #img = np.expand_dims(img, -1)
+        print(img.shape)
         h0, w0 = img.shape[:2]  # orig hw
         r = self.img_size / max(h0, w0)  # resize image to img_size
         if r != 1:  # always resize down, only resize up if training with augmentation
